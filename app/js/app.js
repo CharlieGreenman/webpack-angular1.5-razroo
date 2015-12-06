4fcd43dd1f82451f2bd7c2c9fd4598f65f98fe9a
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-12-06 18:26:01 -0500

    webpack-dev-server works properly

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,3 @@
-var React = require('react');
+var txt = require("./_anim.js") + 'm';
 
-document.addEventListener("DOMContentloaded", domloaded, false);
-
-/** Creates a canvas for loop
- @param {string} title - The title of the book.
- */
-function domloaded(){
-
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-
-    var j = 0, pixSize = 2, pixCount = 50;
-    for(let r = 0; r < pixCount; r++) {
-      for(let i = 0; i < pixCount; i++) {
-        if(i % pixCount === 0){
-          j++;
-        }
-        //random color function
-        ctx.fillStyle = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
-        ctx.fillRect(i * pixSize, j * pixSize, 20, 20);
-        }
-    }
-}
-setInterval(domloaded, 150);
-
-
+alert(txt);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
 var path = require("path");
 
-config = {
-    devtool: 'eval',
-    // Makes sure errors in console map to the correct file
-    // and line number
+module.exports = {
     entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        path.resolve(__dirname, 'app/main.js')
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080/dist',
+        path.resolve(__dirname, 'app/js/app.js')
     ],
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, 'dist/js'),
+        publicPath: 'js',
         filename: 'bundle.js'
-
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-            {test: /\.js$/, exclude: /node_modules/, loader: "eslint-loader" }
-        ]
-    },
-    output: {
-        filename: "./dist/js/app.js"
     }
+//    //module: {
+//    //    preLoaders: [
+//    //        { test: /\.json$/, loader: 'json'}
+//    //    ],
+//    //    loaders: [
+//    //        { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+//    //        {test: /\.js$/, exclude: /node_modules/, loader: "eslint-loader" }
+//    //    ]
+//    //}
 };
-
-module.exports = config;
