4d0b4cbe4cf88eb0e4cc5649b26b753be59a1f4b
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-12-17 00:51:41 -0500

    remove bower dependencies and instead go with npm:

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 var jade = require("../jade/index.jade")();
+require("../scss/core.scss");
+require("jquery");
+require("foundation-sites");
 document.body.innerHTML = jade;
 console.log(jade);
-require("../scss/core.scss");
 var txt = require("./_anim.js") + "le";
 
