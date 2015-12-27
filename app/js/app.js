7c9592d331dbf3fe8b1c3fb4235d966b2adb2499
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-12-26 20:03:33 -0500

    Add babel transforms in package.json,. remove foundation as a scss dependency, remove code that is affecting js compilation, add jade script to allow for initial compilation.

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,4 @@
 //var jade = require("../jade/index.jade")();
 require("../scss/core.scss");
-//require("jquery");
-//require("foundation-sites");
-//document.body.innerHTML = jade;
-//console.log(jade);
-var txt = require("./_anim.js") + "l";
-require("./_test.js");
 
+var txt = require("./_anim.js") + "l";
