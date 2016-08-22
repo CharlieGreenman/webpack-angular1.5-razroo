41aaf47a375b4264c54e1b20aa19e0e1e257e132
Charlie Greenman <charlie@razroo.com>
2016-08-22 13:14:24 -0400

    Add main.js file to new file structure.

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,11 @@
 require("../assets/scss/core.scss");
+
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+angular.module("myApp", [
+    uiRouter,
+    Common.name,
+    Components.name
+])
+.directive("app", AppComponent);
