fad632a2ccb360d230b44c2893af092acd98b621
Charlie Greenman <charie@razroo.com>
2016-08-23 00:41:23 -0400

    Complete angular 1.5 angular app.

--- src/app/main.js
+++ src/app/main.js
@@ -2,7 +2,7 @@ require("../assets/scss/core.scss");
 
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-// import Common from './common/common';
+import AppComponent from './app.component.js';
 import Components from './components/components';
 
 angular.module("myApp", [
