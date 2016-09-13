8257fb4abe860a587a634fcb8d5ff1e07fc558dd
Charlie Greenman <charlie@razroo.com>
2016-09-13 14:00:09 -0400

    Update the home controller to have mapState.

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,15 +1,28 @@
 import styles from '../../../assets/scss/home.scss';
 
 class HomeController {
-  constructor() {
+  constructor($ngRedux) {
     this.name = 'home';
     this.styles = styles;
     this.user = {
       name: 'guest',
       last: 'visitor'
-    }
+    };
+    this.unsubscribe = $ngRedux.connect(this.mapStateToThis, TodoActions)(this);
+  }
+
+  $onDestroy(){
+    this.unsubscribe();
+  }
+
+  mapStateToThis(state) {
+    return {
+      todos: state.todos
+    };
   }
 
 }
+
+HomeController.$inject = ["$ngRedux"];
 
 export default HomeController;
