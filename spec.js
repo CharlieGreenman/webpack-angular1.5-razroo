43a5651e13541bb845075b40f044644fbfd4dc0e
Charlie Greenman <charie@razroo.com>
2016-09-27 23:42:07 -0400

    Add angular mocks to build.

--- spec.js
+++ spec.js
@@ -1,7 +1,6 @@
 var testsContext;
 
-require('babel-core/polyfill');
-require('./vendor/angular.src');
+// require('./vendor/angular.src');
 require('angular-mocks');
 
 testsContext = require.context('./src', true, /_spec\.js$/);
