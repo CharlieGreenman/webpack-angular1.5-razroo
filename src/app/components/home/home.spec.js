d6edbbe593885af091d65ce9b81ffd3fffa9a3d1
Charlie Greenman <charie@razroo.com>
2016-11-10 22:26:42 -0500

    Update package.json to work with mocha config and istanbul alpha. expected etc.

---
+++ src/app/components/home/home.spec.js
@@ -0,0 +1,26 @@
+// import { addToValue10 } from './HomeController';
+// var expect = require('chai').expect;
+
+// import { expect } from 'chai';
+import { assert } from 'chai';
+
+function add() {
+  return Array.prototype.slice.call(arguments).reduce(function(prev, curr) {
+    return prev + curr;
+  }, 0);
+}
+
+describe('add()', function() {
+  var tests = [
+    {args: [1, 2],       expected: 3},
+    {args: [1, 2, 3],    expected: 6},
+    {args: [1, 2, 3, 4], expected: 10}
+  ];
+
+  tests.forEach(function(test) {
+    it('correctly adds ' + test.args.length + ' args', function() {
+      var res = add.apply(null, test.args);
+      assert.equal(res, test.expected);
+    });
+  });
+});
