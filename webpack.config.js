978b102a5d9df3e4e75a5b791961a93645438b99
Charlie Greenman <charie@razroo.com>
2016-12-20 00:32:27 -0500

    Move all loaders over to style-loader resolve to solve instances where it needs to use autprefixer and sass.

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
               extensions: ['', '.js', '.jsx']
           },
           resolveLoader: {
-            modules: ['style-loader', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'],
+            modules: ['style-loader', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', "autoprefixer-loader", "sass-loader"],
           }
         }
       }),
@@ -67,7 +67,7 @@ module.exports = {
           },
           {
             test: /\.scss$/,
-            use: ["style-loader", "autoprefixer-loader", "sass-loader"]
+            use: "style-loader"
           },
           {
             test: /\.css$/,
